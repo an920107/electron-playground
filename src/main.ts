8a870cbe7a9c041a15ef67fbeba549e683884fba
an920107 <an920107@gmail.com>
2023-11-03 01:14:52 +0800

    edit function

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ const createWindow = () => {
     })
 
     win.loadFile(path.join(__dirname, 'index.html'))
-    win.webContents.openDevTools();
+    // win.webContents.openDevTools();
 }
 
 app.whenReady().then(() => {
--- src/renderer.ts
+++ src/renderer.ts
@@ -19,6 +19,34 @@ var todoListView = document.getElementById('todoListView')
 var todoInput = document.getElementById('todoInput') as HTMLInputElement
 var todoButton = document.getElementById('todoButton')
 
+const wrapWithPS2 = (element: HTMLElement) => {
+    const div = document.createElement('div')
+    div.className = 'ps-2'
+    div.append(element)
+    return div
+}
+
+const toggleInput = (element: HTMLDivElement) => {
+    if (element.children.length == 0) {
+        const input = document.createElement('input')
+        input.type = 'text'
+        input.className = 'form-control'
+        input.value = element.innerText
+        input.addEventListener('keypress', (event) => {
+            if (event.key == 'Enter') {
+                toggleInput(element)
+            }
+        })
+        element.innerText = ''
+        element.append(input)
+    } else {
+        const input = element.getElementsByTagName('input')[0]
+        const text = input.value
+        input.remove()
+        element.innerText = text
+    }
+}
+
 const buildListTile = (text: string) => {
     const listTile = document.createElement('div')
     listTile.className = 'col-12 py-1'
@@ -33,15 +61,21 @@ const buildListTile = (text: string) => {
     const textCol = document.createElement('div')
     textCol.className = 'col'
     const buttonCol = document.createElement('div')
-    buttonCol.className = 'col text-end'
-    var button = document.createElement('button')
-    button.className = 'btn btn-danger'
-    button.addEventListener('click',  () => listTile.remove())
-    const icon = document.createElement('i')
-    icon.className = 'fa-solid fa-trash'
-    
-    button.append(icon)
-    buttonCol.append(button)
+    buttonCol.className = 'col-auto d-flex flex-row-reverse'
+    var editButton = document.createElement('button')
+    editButton.className = 'btn btn-secondary'
+    const editIcon = document.createElement('i')
+    editIcon.className = 'fa-solid fa-pen-to-square'
+    var deleteButton = document.createElement('button')
+    deleteButton.className = 'btn btn-danger'
+    const deleteIcon = document.createElement('i')
+    deleteIcon.className = 'fa-solid fa-trash'
+
+    editButton.append(editIcon)
+    editButton.addEventListener('click', () => toggleInput(textCol))
+    deleteButton.append(deleteIcon)
+    deleteButton.addEventListener('click', () => listTile.remove())
+    buttonCol.append(wrapWithPS2(deleteButton), wrapWithPS2(editButton))
     textCol.textContent = text
     row.append(textCol, buttonCol)
     blockquote.append(row)
